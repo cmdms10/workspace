a39162504ed4a4f0da2d6d7cbb9730b2e3a5ddfe
ponsima <ponsima1140@gmail.com>
2018-09-25 23:35:14 +0900

    整数どうしの割り算

---
+++ Java/src/CastExample.java
@@ -0,0 +1,9 @@
+public class CastExample {
+    public static void main(String args[]) {
+        int i = 5;
+        int k = 2;
+        double c = (double)i / (double)k;
+
+        System.out.println(c);
+    }
+}

dca6e009f020302c03ab1d0a1d620178b981d476
ponsima <ponsima1140@gmail.com>
2018-09-25 22:11:04 +0900

    変数の出力サンプル２

---
+++ Java/src/VariableExample2.java
@@ -0,0 +1,13 @@
+public class VariableExample2 {
+    public static void main(String args[]) {
+        int i = 5;
+        System.out.println(i);
+
+        i = 10;
+        System.out.println(i);
+
+        int j = i;
+        System.out.println(j);
+
+    }
+}

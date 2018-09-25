f94809eaaa46a0c9830409f07ff23956237a8549
ponsima <ponsima1140@gmail.com>
2018-09-25 22:17:17 +0900

    計算をするサンプルプログラム

---
+++ Java/src/CalcExample.java
@@ -0,0 +1,11 @@
+public class CalcExample {
+    public static void main(String args[]) {
+        System.out.println(3+6);
+
+        int i = 4;
+        int j = i + 7;
+        int k = j + i * 3;
+
+        System.out.println(k);
+    }
+}

7ba8c3fedddddfba1d1c6f4ef710aea90bbf7ad4
ponsima <ponsima1140@gmail.com>
2018-09-25 23:55:31 +0900

    if~else文

---
+++ Java/src/ElseExample.java
@@ -0,0 +1,10 @@
+public class ElseExample {
+    public static void main(String args[]) {
+        int age = 20;
+        if(age < 20) {
+            System.out.println("未成年ですね。");
+        }else{
+            System.out.println("２０歳以上ですね。");
+        }
+    }
+}

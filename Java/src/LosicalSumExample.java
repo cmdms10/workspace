54a0b9f56e2461c5bd9d74e355de3bb1ae7a3c4f
ponsima <ponsima1140@gmail.com>
2018-09-26 23:54:17 +0900

    論理演算子練習

--- Java/src/LosicalSumExample.java
+++ Java/src/LosicalSumExample.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class LosicalSumExample {
+    public static void main(String args[]) {
+        int age = 20;
+        if(age < 13 || age >= 65) {
+            System.out.println("入園料は無料です");
+        }else
+            System.out.println("料金が必要です");
+    }
+    
 }

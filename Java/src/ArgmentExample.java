64f76771bb1ea4ce33dfd9488a45f5658bfa1052
ponsima <ponsima1140@gmail.com>
2018-09-29 00:44:12 +0900

    mainメソッドの引数

---
+++ Java/src/ArgmentExample.java
@@ -0,0 +1,8 @@
+public class ArgmentExample {
+    public static void main(String[] args) {
+        System.out.println("引数で渡された配列の要素数" + args.length);
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}

4c675e728b4fb34d234c29c9e4f4097a5b59d0b0
ponsima <ponsima1140@gmail.com>
2018-09-29 14:59:19 +0900

    オーバーロードができない場合
    変数の名前や戻り値が異なるだけではオーバーロードできない。

---
+++ Java/src/OverExample2.java
@@ -0,0 +1,13 @@
+public class OverExample2 {
+    public static void methodA(String i) {
+        System.out.println(i);
+    }
+    public static void methodB(String j) {
+        System.out.println(j);
+    }
+
+    public static void main(String[] args) {
+        methodA("hogehoge");
+        methodB("hoguhogu");
+    }
+}

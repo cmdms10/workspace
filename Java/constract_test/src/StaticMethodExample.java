2ecedf1c65bf736dd7545e12b57651299bb47735
ponsima <ponsima1140@gmail.com>
2018-09-30 12:46:03 +0900

    クラスメソッド

---
+++ Java/constract_test/src/StaticMethodExample.java
@@ -0,0 +1,5 @@
+public class StaticMethodExample {
+    public static void main(String[] args) {
+        System.out.println("底辺が10、高さが5の三角形の面積は" + SimpleCalc.getTriangleArea(10.0, 5.0)+"です");
+    }
+}

812d653d3d1e006accbe592dd6b9d0c422a6c2d2
ponsima <ponsima1140@gmail.com>
2018-09-30 12:42:50 +0900

    studentCardクラス作成

---
+++ Java/sample1/src/StudentCard.java
@@ -0,0 +1,30 @@
+class StudentCard {
+
+    //クラス変数(フィールド(情報)の宣言)
+    static int counter = 0;
+
+    //インスタンス変数(フィールド(情報)の宣言)
+    int id; //学籍番号
+    String name; //氏名
+
+    //コンストラクタ追加
+    StudentCard(int id, String name) {
+        this.id = id;
+        this.name = name;
+        StudentCard.counter++;
+    }
+
+    //インスタンスメソッドの宣言(メソッド(機能)の宣言)
+    void printInfo() {
+        System.out.println("学籍番号："+this.id);
+        System.out.println("氏名："+this.name);
+        System.out.println(StudentCard.counter);
+    }
+
+    //クラスメソッドの宣言(メソッド(機能)の宣言)
+    static double getTriangleArea(double base, double height) {
+        return base * height / 2;
+    }
+}
+
+

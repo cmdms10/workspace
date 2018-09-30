ad735f526239373e1df1948d4351e7ac73786613
ponsima <ponsima1140@gmail.com>
2018-09-30 16:57:17 +0900

    StudentCardクラス更新

---
+++ Java/src/StudentCard.java
@@ -0,0 +1,10 @@
+class StudentCard {
+    int id; //学生番号
+    String name; //氏名
+
+    void printInfo() {
+        System.out.println("学籍番号："+this.id);
+        System.out.println("氏名："+this.name);
+    }
+}
+

278a8b157f71a42093d29eb090fc9c241e08dca8
ponsima <ponsima1140@gmail.com>
2018-10-02 11:19:52 +0900

    入力処理

---
+++ JV21/src/Sample4_1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Sample4_1 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("文字列を入力してください");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str+"が入力されました");
+    }
+
+}

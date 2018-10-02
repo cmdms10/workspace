4c43a675cf5e79713c313e45c74abf2a1a39b347
ponsima <ponsima1140@gmail.com>
2018-10-02 11:20:35 +0900

    変数・複数変数

---
+++ JV21/src/Sample3_1.java
@@ -0,0 +1,17 @@
+public class Sample3_1 {
+    public static void main(String[] args) {
+        int num ;
+        num = 3;
+
+        System.out.println("変数numの値は" + num +"です。");
+
+        num += 3;
+
+        System.out.println(num);
+
+        int num1,num2;
+        num1 = 5;
+        num2 = 8;
+        System.out.println(num2-num1);
+    }
+}

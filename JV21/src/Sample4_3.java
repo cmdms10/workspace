ea9cebc87cfe84f2c7a474d3dba96ff7223aea1a
ponsima <ponsima1140@gmail.com>
2018-10-16 11:09:10 +0900

    先生コード追加

--- JV21/src/Sample4_3.java
+++ JV21/src/Sample4_3.java
@@ -4,7 +4,7 @@ public class Sample4_3 {
     public static void main(String[] args) throws IOException {
         System.out.println("整数を３つ入力してください。");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str1 = br.readLine();
+/*        String str1 = br.readLine();
         int num1 = Integer.parseInt(str1);
 
         String str2 = br.readLine();
@@ -14,6 +14,17 @@ public class Sample4_3 {
         int num3 = Integer.parseInt(str3);
 
         int sum = num1+num2+num3;
+        System.out.println("３つの数の合計は"+sum+"です");*/
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        String str3 = br.readLine();
+
+        int sum = 0;
+        sum += Integer.parseInt(str1);
+        sum += Integer.parseInt(str2);
+        sum += Integer.parseInt(str3);
+
         System.out.println("３つの数の合計は"+sum+"です");
     }
 }

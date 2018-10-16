ace540511a1cd6a6aa70bdfc338d188837fea399
ponsima <ponsima1140@gmail.com>
2018-10-16 10:46:21 +0900

    値の出力

---
+++ JV21/src/Sample4_2.java
@@ -0,0 +1,16 @@
+public class Sample4_2 {
+    public static void main(String[] args) {
+        /*式の値を出力*/
+        System.out.println("1+2は"+(1+2)+"です。");
+        System.out.println("3+4は"+(3+4)+"です。");
+
+        /*変数を使って値を出力*/
+        int num1 = 10;
+        int num2 = 5;
+        System.out.println("num1+num2は"+(num1+num2)+"です。");
+        System.out.println("num1-num2は"+(num1-num2)+"です。");
+        System.out.println("num1*num2は"+(num1*num2)+"です。");
+        System.out.println("num1%num2は"+(num1/num2)+"です。");
+        System.out.println("num1%num2の余りは"+(num1%num2)+"です。");
+    }
+}

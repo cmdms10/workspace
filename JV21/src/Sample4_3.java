f19981e4564707e9ef82e59666fc0ef0d8fb62c7
ponsima <ponsima1140@gmail.com>
2018-10-16 11:01:29 +0900

    値の出力２

--- JV21/src/Sample4_3.java
+++ JV21/src/Sample4_3.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.*;
 
 public class Sample4_3 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("整数を３つ入力してください。");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        int num1 = Integer.parseInt(str1);
+
+        String str2 = br.readLine();
+        int num2 = Integer.parseInt(str2);
+
+        String str3 = br.readLine();
+        int num3 = Integer.parseInt(str3);
+
+        int sum = num1+num2+num3;
+        System.out.println("３つの数の合計は"+sum+"です");
+    }
 }

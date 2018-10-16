2fc2ff1c5e83ec8a130918effc46e1fb904b1ae3
ponsima <ponsima1140@gmail.com>
2018-10-16 12:17:04 +0900

    キャスト

---
+++ JV21/src/Sample4_6.java
@@ -0,0 +1,9 @@
+public class Sample4_6 {
+    public static void main(String[] args) {
+        /*intとintの型変換*/
+        int num1 = 5;
+        int num2 = 4;
+        double div = (double)num1/num2;
+        System.out.println("5÷4は"+div+"です。");
+    }
+}

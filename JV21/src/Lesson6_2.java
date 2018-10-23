8405bd5a4b43ea60640719c3b222114fb999f802
ponsima <ponsima1140@gmail.com>
2018-10-23 12:00:38 +0900

    三角形

---
+++ JV21/src/Lesson6_2.java
@@ -0,0 +1,11 @@
+public class Lesson6_2 {
+    public static void main(String[] args) {
+        /*三角形*/
+        for(int i=0; i<10; i++) {
+            for(int k=0; k<=i; k++) {
+                System.out.print("※");
+            }
+            System.out.println("\t");
+        }
+    }
+}

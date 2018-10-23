47861aae67939e9faa900ab1c4e03cfcab8878d9
ponsima <ponsima1140@gmail.com>
2018-10-23 12:00:19 +0900

    九九表

---
+++ JV21/src/Lesson6_1.java
@@ -0,0 +1,11 @@
+public class Lesson6_1 {
+/*九九表*/
+    public static void main(String[] args) {
+        for(int i=1; i<10; i++) {
+            for(int k=1; k<10; k++) {
+                System.out.print(i*k + "\t");
+            }
+            System.out.println("\n");
+        }
+    }
+}

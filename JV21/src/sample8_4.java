dc734bca418b3aafa3085ba999f241bab9827fa0
ponsima <ponsima1140@gmail.com>
2018-11-06 12:57:47 +0900

    クラス2

--- JV21/src/sample8_4.java
+++ JV21/src/sample8_4.java
@@ -1,3 +1,10 @@
 public class sample8_4 {
+    public static void main(String[] args) {
+        Car car1 = new Car();
 
+        car1.setNum(1234);
+        car1.setGas(20.5);
+
+        car1.show();
+    }
 }

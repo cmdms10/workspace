4c9614296e868a7cdae38baf8fa6fea0478294c6
ponsima <ponsima1140@gmail.com>
2018-11-06 12:36:21 +0900

    クラス

---
+++ JV21/src/sample8_1.java
@@ -0,0 +1,33 @@
+class Car {
+    int num;
+    double gas;
+
+    void show() {
+        System.out.println("車のナンバーは"+num+"です。");
+        System.out.println("ガソリンの量は"+gas+"です。");
+    }
+}
+
+public class sample8_1 {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        car1.num = 1234;
+        car1.gas = 20.5;
+
+//        System.out.println("車のナンバーは"+car1.num+"です。");
+//        System.out.println("ガソリンの量は"+car1.gas+"です。");
+        car1.show();
+
+
+        car2.num = 1235;
+        car2.gas = 12.3;
+
+//        System.out.println("車のナンバーは"+car2.num+"です。");
+//        System.out.println("ガソリンの量は"+car2.gas+"です。");
+
+        car2.show();
+
+    }
+}

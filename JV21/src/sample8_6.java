4274ac7d02d1f6ae0289f970eed044b1cafd34b0
ponsima <ponsima1140@gmail.com>
2018-11-20 11:51:29 +0900

    sample8_6 コメントアウト

---
+++ JV21/src/sample8_6.java
@@ -0,0 +1,17 @@
+/*public class sample8_6 {
+    public void main(String[] args) {
+        Car car1 = new Car();
+
+        car1.setNumGas(1234,20.5);
+
+//        getメソッドを使ってナンバーを知る
+        int number = car1.getNum();
+//        getメソッドを使っていガソリンの量を知る
+        double gasoline = car1.getGas();
+
+        *//*表示用メソッドを使ってナンバーとガソリンを出力*//*
+        car1.show();
+//        numberとgasolineを使ってい設定を表示
+        System.out.println("ナンバーは"+number+"です。ガソリンは"+gasoline+"でした");
+    }
+}*/
